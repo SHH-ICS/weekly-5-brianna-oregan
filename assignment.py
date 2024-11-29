1d453d8efa66e952132492a5a75f4096d4e9c90b
Brianna O'Regan <brianna.oregan@stu.ocsb.ca>
2024-11-29 15:13:04 +0000

    add assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,14 @@
+#def calculatePi(digits):
+ # result = 0
+  #return result
+
+#number_of_digits = input()
+#print(calculatePi(number_of_digits))
+
 def calculatePi(digits):
-  result = 0
+  result = sum(4 * (-1) ** i / (2 * i + 1) for i in range(digits))
   return result
 
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+number_of_digits = int(input()) 
+pi = calculatePi(number_of_digits)  
+print(pi)
